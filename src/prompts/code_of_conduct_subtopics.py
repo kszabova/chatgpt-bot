3b0001e9f5c518bd8551c9546f281581babdd10c
Kristina Szabova <k.szabova98@gmail.com>
2023-05-07 19:21:33 +0200

    Create prompts for ChatGPT

---
+++ src/prompts/code_of_conduct_subtopics.py
@@ -0,0 +1,516 @@
+CHAP_00 = """Obchodné praktiky spoločnosti
+Nestlé sú od jej samotného založenia
+postavené na bezúhonnosti,
+úprimnosti, poctivom obchodnom
+styku a dôslednom dodržiavaní
+všetkých platných zákonov. Odvtedy
+zamestnanci Nestlé po celom
+svete vždy dodržiavali tieto záväzky
+pri plnení svojich každodenných
+povinností a povesť Nestlé je dnes
+jedným z najdôležitejších aktív tejto
+spoločnosti.
+Zásady podnikania Nestlé
+predpisujú určité hodnoty
+a princípy, ktoré spoločnosť Nestlé
+dodržiava na celom svete. Tento
+Kódex obchodného správania
+upresňuje Zásady podnikania
+Nestlé a napomáha ich zavádzaniu
+do praxe tým, že stanovuje určité
+trvalé minimálne normy správania
+v kľúčových oblastiach.
+Tento Kódex nie je určený na
+to, aby pokrýval všetky situácie,
+ktoré môžu nastať. Je navrhnutý,
+aby poskytoval obecný rámec,
+podľa ktorého bude možné hodnotiť
+akékoľvek činnosti. Ak majú
+zamestnanci pochybnosti o vhodnom
+správaní v danej situácii, mali by
+v Kódexu hľadať radu či návod,
+pretože konečná zodpovednosť za
+„správne konanie“ je na každom
+zamestnancovi a nie je možné ju
+delegovať na niekoho iného.
+Zamestnanci by sa mali vždy riadiť
+týmito základnými pravidlami:
+- vyhnúť sa konaniu, ktoré by mohlo
+poškodiť alebo ohroziť spoločnosť
+Nestlé alebo jej dobrú povesť;
+- konať čestne a v medziach
+zákona;
+- klásť záujmy spoločnosti nad svoje
+vlastné osobné a iné záujmy.
+Pre účely tohto kódexu odkazy na
+„zamestnanca“ znamenajú/zahŕňajú
+tiež odkazy na zamestnancov,
+spoločníkov, vedúcich pracovníkov
+a riaditeľov spoločnosti Nestlé S.A.
+a jej dcérskych spoločností."""
+
+CHAP_01 = """Vždy dodržiavame zákony
+
+Spoločnosť Nestlé a jej zamestnanci
+sú viazaní zákonom. Dodržiavanie
+všetkých zákonov a predpisov
+nesmie byť nikdy spochybňované.
+Okrem toho sú zamestnanci povinní
+dodržiavať vnútorné predpisy
+a nariadenia platné pre danú situáciu.
+Tieto interné predpisy sú špecifi cké
+pre našu spoločnosť a môžu
+presahovať požiadavky stanovené
+zákonmi."""
+
+CHAP_02 = """Konáme vždy v najlepšom záujme
+spoločnosti Nestlé
+
+Konflikt záujmov nastáva vtedy, ak si
+osobné záujmy zamestnanca alebo
+záujmy tretej strany konkurujú so
+záujmami spoločnosti Nestlé. V takej
+situácii môže byť pre zamestnanca
+ťažké konať úplne v najlepšom záujme
+pre spoločnosť Nestlé.
+Zamestnanec sa bude vždy,
+ak je to možné, vyhýbať konfl iktu
+záujmov. Ak nastane situácia konfl iktu
+záujmov, alebo ak zamestnanec čelí
+situácii, ktorá môže zahŕňať alebo
+viesť ku konfl iktu záujmov, oznámi
+túto skutočnosť svojmu priamemu
+nadriadenému alebo oddeleniu
+ľudských zdrojov alebo vedúcemu
+právneho oddelenia či manažérovi pre
+dodržiavanie pravidiel (Compliance
+Offi cer), aby vyriešili danú situáciu
+poctivým a transparentným
+spôsobom."""
+
+CHAP_03 = """Sme hrdí na dobré meno Nestlé
+a berieme ohľad na najlepšie
+záujmy Nestlé pri našich externých
+záväzkoch a aktivitách
+
+Žiadne činnosti sa nesmú
+vykonávať mimo spoločnosti
+Nestlé, ak by boli v rozpore zo
+zodpovednosťou zamestnanca
+voči spoločnosti Nestlé alebo
+predstavovali riziko pre povesť
+spoločnosti Nestlé, alebo by boli iným
+spôsobom v rozpore so záujmami
+Nestlé.
+Ak má zamestnanec pochybnosti
+o tom, či je určitá činnosť prípustná,
+je povinný prerokovať túto záležitosť
+s vedúcim oddelenia ľudských
+zdrojov alebo s vedúcim právneho
+oddelenia či s manažérom pre
+dodržiavanie pravidiel (Compliance
+Offi cer).
+Nasledujúce funkcie a činnosti
+sa považujú za prijateľné, iba ak
+boli vopred schválené štatutárnym
+orgánom spoločnosti Nestlé:
+- člen orgánu spoločnosti
+- vedúci pracovník
+- zamestnanec
+- partner
+- poradca
+Schválenie nebude vydané,
+ak je pravdepodobné, že by táto
+funkcia alebo činnosť bola v rozpore
+so záujmami spoločnosti Nestlé
+alebo so zodpovednosťou daného
+zamestnanca.
+Členstvo v správnych radách
+verejne obchodovateľných
+spoločností vyžaduje, aby mu
+predchádzal súhlas generálneho
+riaditeľa Nestlé a ak ide o členov
+správnej rady, tak súhlas predsedu
+správnej rady Nestlé.
+Ak o prijatie určitej funkcie alebo
+činnosti nepožiada zamestnanca
+priamo sama spoločnosť Nestlé,
+budú zamestnanci vykonávať externé
+činnosti a zastávať funkcie na vlastné
+riziko a náklady a iba vo svojom
+voľnom čase."""
+
+CHAP_04 = """Naše rozhodnutia o prijímaní
+a pracovnom rozvoji pracovníkov
+budú spravodlivé a objektívne.
+
+Priami členovia rodiny a partneri
+zamestnancov sa môžu prijímať do
+pracovného pomeru ako zamestnanci
+alebo poradcovia za predpokladu,
+že prijatie je založené na kvalifi kácii,
+výkone, znalostiach a skúsenostiach,
+a že neexistuje priama ani nepriama
+podriadenosť medzi zamestnancom
+a jeho/jej príbuzným či partnerom.
+Tieto zásady spravodlivej
+zamestnaneckej politiky platia pre
+všetky aspekty zamestnania, vrátane
+náhrad, povýšení a prevedení, ako
+aj v prípade, že príbuzenský vzťah
+vznikne až potom, čo príslušný
+zamestnanec nastúpil do spoločnosti.
+Za predpokladu, že deti
+zamestnancov spoločnosti Nestlé
+budú rovnako vhodnými kandidátmi
+ako ostatní uchádzači, môžu byť
+uprednostnené v prípade, ak ide
+o študentskú prax, zaškolenie,
+zamestnanie počas prázdnin
+a podobné krátkodobé plnenia úloh."""
+
+
+CHAP_05 = """Naším záväzkom je podporovať
+podnikanie spoločnosti Nestlé.
+
+Zamestnanci nebudú spoločnosti
+konkurovať a ani nevyužijú osobné
+výhody z obchodných príležitostí,
+ktoré sa im naskytnú počas ich
+pracovného pomeru okrem prípadu,
+ak spoločnosť výslovne uvedie, že
+nemá záujem o využitie uvedenej
+obchodnej príležitosti.
+Ak chce zamestnanec využiť
+obchodnú príležitosť, ktorá by
+mohla spoločnosť zaujímať, oznámi
+túto skutočnosť svojmu priamemu
+nadriadenému, ktorý preverí, či
+má spoločnosť záujem využiť
+uvedenú obchodnú príležitosť.
+Ak sa spoločnosť rozhodne, že sa
+nebude touto príležitosťou zaoberať,
+zamestnanec môže túto príležitosť
+využiť vo svoj prospech iba vtedy, ak
+je jasné, že využitie tejto príležitosti
+nebude priamo či nepriamo
+konkurovať podnikaniu spoločnosti."""
+
+CHAP_06 = """Rešpektujeme Pravidla
+o obchodovaní akciami medzi
+zasvätenými osobami (tzv. insider
+trading) a riadime sa nimi pri
+nákupe a predaji cenných papierov
+Nestlé.
+
+Spoločnosť Nestlé zakazuje nákup
+a predaj akcií alebo cenných
+papierov Nestlé na základe informácií
+s možným dopadom na cenu akcií,
+ktoré ešte neboli publikované.
+Nedodržanie týchto predpisov
+nielenže môže viesť k disciplinárnemu
+konaniu, ale i k obvineniu z trestného
+činu.
+Ak existuje pochybnosť
+o interpretácii alebo použiteľnosti
+Pravidiel o obchodovaní akciami
+medzi zasvätenými osobami (tzv.
+insider trading), zamestnanci budú
+kontaktovať vedúceho právneho
+oddelenia alebo manažéra pre
+dodržiavanie pravidiel (Compliance
+Offi cer)."""
+
+CHAP_07 = """Veríme v dôležitosť voľnej
+konkurencie.
+
+Spoločnosť Nestlé je pripravená
+úspešne konkurovať v dnešnom
+podnikateľskom prostredí a bude
+pri tom vždy postupovať v súlade
+so všetkými platnými zákonmi
+na ochranu hospodárskej súťaže
+a proti nekalej súťaži. Preto musia
+zamestnanci vždy dodržiavať
+nasledujúce zásady:
+- Obchodná politika a ceny sa
+stanovia nezávisle a nikdy nie na
+základe formálnej či neformálnej
+dohody s konkurenciou alebo s inými
+stranami, priamo ani nepriamo;
+- Spoločnosť Nestlé si nebude
+nikdy so svojimi konkurentmi
+navzájom rozdeľovať zákazníkov,
+teritóriá alebo trhy produktov. Toto
+rozdelenie bude vždy výsledkom
+čestného konkurenčného súperenia;
+- So zákazníkmi a dodávateľmi sa
+bude rokovať poctivo a spravodlivo.
+Všetci zamestnanci, ale hlavne
+tí, ktorí sa zaoberajú marketingom,
+predajom a nákupom, alebo
+ktorí prichádzajú do pravidelného
+kontaktu s konkurenciou, sú povinní
+zoznámiť sa s platnými zákonmi na
+ochranu hospodárskej súťaže. Ak
+dôjde k pochybnostiam, sú povinní
+kontaktovať právne oddelenie, ktoré
+im poskytne informácie a školenie
+ohľadne zákona na ochranu
+hospodárskej súťaže."""
+
+CHAP_08 = """Ceníme si a chránime naše
+dôverné informácie a rešpektujeme
+dôverné informácie ostatných.
+
+Dôverná informácia je akákoľvek
+informácia, ktorá nie je verejne
+dostupná alebo ešte nebola
+oznámená verejnosti. Medzi
+dôverné informácie patria obchodné
+tajomstvá, obchodné a marketingové
+plány a plány služieb, spotrebiteľské
+prieskumy, technické a výrobné
+návrhy, receptúry, návrhy dizajnov,
+databáze, záznamy, informácie
+o platoch a všetky nepublikované
+fi nančné alebo iné údaje.
+Trvalý úspech spoločnosti Nestlé
+závisí od využívania dôverných
+informácií a ich nezverejňovania
+tretím osobám. Zamestnanci
+nezverejnia dôverné informácie
+a ani neumožnia ich zverejnenie, ak
+toto zverejnenie nevyžaduje zákon,
+alebo ak neboli oprávnení vedením
+spoločnosti. Tento záväzok trvá aj
+po ukončení pracovného pomeru.
+Zamestnanci musia urobiť všetko
+preto, aby neúmyselne neprezradili
+dôvernú informáciu, a preto budú
+dôverné informácie ukladať,
+skladovať alebo posielať so zvýšenou
+opatrnosťou.
+Spoločnosť Nestlé rešpektuje, že
+tretie strany majú podobný záujem
+chrániť svoje dôverné informácie. Ak
+tretie strany, ako partneri spoločného
+podniku, dodávatelia alebo zákazníci
+oboznámia spoločnosť Nestlé so
+svojimi dôvernými informáciami,
+Nestlé bude s nimi zaobchádzať,
+ako by to boli jej vlastné dôverné
+informácie. Rovnakým spôsobom
+budú zamestnanci chrániť dôverné
+informácie, ktoré získali počas svojho
+bývalého zamestnania."""
+
+CHAP_09 = """Trváme na poctivosti a ceníme si
+aktíva a majetok spoločnosti.
+
+Zamestnanci sa nesmú nikdy
+zapliesť do podvodného alebo iným
+spôsobom nečestného správania,
+ktorého predmetom sú aktíva,
+majetok, fi nančné výkazy alebo
+účtovníctvo spoločnosti Nestlé
+alebo inej tretej strany. Nedodržanie
+týchto predpisov nielenže môže viesť
+k disciplinárnemu konaniu, ale až
+k obvineniu z trestného činu.
+Finančné záznamy spoločnosti
+Nestlé sú základom pre riadenie
+obchodnej činnosti spoločnosti
+a plnenie jej záväzkov voči
+akcionárom a ďalším zainteresovaným
+stranám. Preto musí byť každý
+fi nančný záznam presný a v súlade
+s účtovnými normami spoločnosti
+Nestlé.
+Zamestnanci budú chrániť
+majetok spoločnosti Nestlé
+a budú ho využívať len riadne
+a zodpovedajúcim spôsobom.
+Všetci zamestnanci sa budú snažiť
+chrániť majetok Nestlé pred stratou,
+poškodením, nesprávnym použitím,
+krádežou, podvodom, spreneverou
+a zničením. Tieto záväzky sa týkajú
+hmotných ako aj nehmotných aktív,
+obchodných značiek, know-how,
+dôverných a chránených informácií
+a informačných systémov.
+V rozsahu danom platným
+zákonom si spoločnosť Nestlé
+vyhradzuje právo sledovať
+a kontrolovať, ako zamestnanci
+využívajú jej aktíva, vrátane kontroly
+všetkých e-mailov, dát a súborov
+uložených na dátových nosičoch
+spoločnosti Nestlé."""
+
+CHAP_10 = """Odsudzujeme každú formu
+podplácania a korupcie.
+
+Zamestnanci nesmú priamo ani
+sprostredkovane ponúkať alebo
+sľubovať akúkoľvek osobnú alebo
+fi nančnú či inú výhodu s cieľom získať
+alebo si udržať obchod či inú výhodu
+od súkromnej alebo verejnej tretej
+strany. Nesmú ani prijímať takúto
+výhodu na oplátku za preferenčné
+zaobchádzanie s treťou stranou.
+Zamestnanci sa tiež musia zdržať
+každej činnosti alebo správania,
+ktoré by mohlo vyvolať dojem alebo
+podozrenie na také konanie alebo
+o pokus o ne.
+Zamestnanci by si mali uvedomiť,
+že ponúkanie alebo poskytovanie
+výhod s cieľom ovplyvniť rozhodnutie
+prijímajúceho tejto výhody, aj keď
+prijímajúci nie je verejný činiteľ,
+môže nielen viesť k disciplinárnemu
+konaniu, ale až k obvineniu
+z trestného činu. Výhoda môže
+predstavovať čokoľvek cenného pre
+príjemcu, vrátane zamestnania alebo
+zmlúv o poradenstve pre spriaznené
+strany.
+Zamestnanci si musia uvedomiť,
+že volebné zákony v mnohých
+jurisdikciách zakazujú, aby podniky
+poskytovali príspevky politickým
+stranám alebo kandidátom.
+Spoločnosť Nestlé prijala predpisy,
+na základe ktorých nie je možné
+príspevky poskytovať, s výnimkou
+materskej fi rmy v krajine, kde sídli
+jej ústredie. Všetky príspevky ako aj
+akékoľvek odchýlky od tejto politiky
+musí schváliť generálny riaditeľ Nestlé
+SA alebo predseda správnej rady
+Nestlé SA. """
+
+CHAP_11 = """Konkurujeme a podnikáme na
+základe kvality a schopností.
+
+Zamestnanci sa nenechajú
+ovplyvňovať prijímaním darov alebo
+pozorností, ani sa nebudú snažiť
+ovplyvňovať iných ich poskytovaním.
+Zamestnanec môže prijať alebo
+ponúknuť primerané pohostenie
+a symbolické dary, ktoré sú vhodné
+za daných okolností a nebude prijímať
+ani ponúkať žiadne dary, pohostenie
+a zábavu, ak by prijatie uvedeného
+mohlo vytvoriť dojem neprimeraného
+ovplyvňovania obchodného vzťahu.
+Pri posudzovaní danej situácie
+budú zamestnanci dodržiavať
+predpisy platné pre danú krajinu.
+Ak také predpisy nie sú k dispozícii,
+budú sa riadiť najprísnejšími
+miestnymi praktikami, aby sa vyhli
+čo len podozreniu nevhodného
+správania. Ak má zamestnanec
+pochybnosti, vyhľadá svojho
+priameho nadriadeného alebo
+pracovníka právneho oddelenia alebo
+manažéra pre dodržiavanie pravidiel
+(Compliance Offi cer).
+Žiadny zamestnanec neponúkne
+tretej strane ani od nej neprijme dary,
+ktoré patria do nasledujúcej skupiny,
+a to bez ohľadu na to, v akej sú
+hodnote:
+- peniaze
+- pôžičky
+- provízie, úplatky
+- obdobné peňažné výhody"""
+
+CHAP_12 = """Vítame rôznorodosť
+a rešpektujeme osobnú dôstojnosť
+svojich kolegov.
+
+Spoločnosť Nestlé rešpektuje
+dôstojnosť, súkromie a osobné
+práva každého zamestnanca a jej
+záväzkom je chrániť pracovisko
+pred diskrimináciou a obťažovaním.
+Preto zamestnanci nesmú nikoho
+diskriminovať na základe pôvodu,
+národnosti, viery, rasy, pohlavia, veku
+alebo sexuálnej orientácie, ani sa
+zapájať do žiadneho verbálneho alebo
+fyzického obťažovania na základe
+niektorých z vyššie uvedených príčin
+ani z inej príčiny.
+Zamestnanci, ktorí majú pocit, že
+ich pracovisko nie je v súlade s vyššie
+uvedenými zásadami, nech neváhajú,
+a oznámia svoje obavy oddeleniu
+ľudských zdrojov."""
+
+CHAP_13 = """Budeme dodržiavať tento
+Kódex, plniť jeho ustanovenia
+a vyhľadáme poradenstvo, ak je to
+potrebné.
+
+Každý zamestnanec je zodpovedný
+za úplné dodržiavanie všetkých
+ustanovení tohto Kódexu a aby
+v prípade potreby požiadal o radu
+svojho priameho nadriadeného,
+oddelenie ľudských zdrojov, právne
+oddelenie alebo manažéra pre
+dodržiavanie pravidiel (Compliance
+Offi cer). Vykonávať veci správne
+v súlade s Kódexom je osobná
+zodpovednosť každého zamestnanca,
+ktorá nemôže byť delegovaná.
+Ak má zamestnanec pochybnosti,
+vždy sa má riadiť základnými
+zásadami uvedenými v úvode tohto
+Kódexu.
+Akékoľvek nedodržiavanie
+tohto Kódexu môže viesť nielen
+k disciplinárnemu konaniu ale aj
+k možnosti ukončenia pracovného
+pomeru, prípadne súdnemu stíhaniu
+alebo trestným sankciám."""
+
+CHAP_14 = """Sme zodpovední za to, aby sme
+vo všetkých situáciách konali
+bezúhonne.
+
+Zamestnanci nahlásia svojmu
+priamemu nadriadenému alebo
+príslušným pracovníkom oddelenia
+ľudských zdrojov, právneho oddelenia
+alebo manažérovi pre dodržiavanie
+pravidiel (Compliance Offi cer)
+všetky praktiky a činnosti, o ktorých
+sa domnievajú, že nezodpovedajú
+zásadám tohto Kódexu alebo sú
+dokonca nezákonné. S ohľadom na
+povahu zistenej záležitosti a ak je to
+primerané, hlásenie o porušovaní
+Kódexu sa môže odovzdávať priamo
+vyšším úrovniam vedenia vrátane
+generálneho riaditeľa skupiny Nestlé
+a riaditeľa Nestlé SA pre dodržiavanie
+pravidiel (Chief Compliance Offi cer).
+Tam, kde je to vhodné,
+sa sťažnosti môžu podávať
+dôverne alebo pomocou horúcej
+zamestnaneckej linky.
+Všetky sťažnosti budú riadne
+vyšetrené. Spoločnosť Nestlé
+zakazuje odvetné opatrenia voči
+zamestnancovi, ktorý podal túto
+správu v dobrej viere, a taktiež chráni
+práva inkriminovanej osoby."""
---
+++ src/prompts/prompts.py
@@ -0,0 +1,47 @@
+SYSTEM_INSTRUCT = (
+    "Jmenuješ se Adéla. "
+    "Jsi chatbot na náborovém oddělení ve společnosti Nestlé. "
+    "Jsi nápomocná a přátelská k uživatelům. "
+    "Snažíš se maximalizovat počet uchazečů o práci ve společnosti Nestlé. "
+    "Mluvíš česky."
+)
+
+SYSTEM_ONE_WORD = "Odpověz jedním slovem ano nebo ne."
+
+SYSTEM_NUMBER = "Odpověz jenom jedním číslem a žádným textem."
+
+SYSTEM_LANGUAGE = "Text je ve slovenštině, ale odpověz česky."
+
+SENTENCE_ANSWER = 'Věta: "{sentence}"\n' "Odpověď: "
+
+TOPIC_CODE_OF_CONDUCT = "Týká se tahle věta kodexu správání ve společnosti Nestlé?\n"
+
+TOPIC_BOT_PERSONALITY = "Týká se tahle věta tvý osobnosti, preferencí nebo zálib?\n"
+
+SUBTOPIC_CODE_OF_CONDUCT = (
+    "Napiš, které z nasledujících tém se týká tahle věta.\n"
+    "0. Obecné informace o kodexu správání ve společnosti Nestlé.\n"
+    "1. Dodržování zákonú, předpisú a nařízení\n"
+    "2. Konflikt zájmů\n"
+    "3. Ředitelské funkce a aktivity mimo společnosti Nestlé\n"
+    "4. Rodiny a rodinní příslušníci\n"
+    "5. Příležitosti pro rozvoj podnikání\n"
+    "6. Obchodování s akcemi mezi zasvěcenými osobami\n"
+    "7. Ochrana hospodárské soutěže a zásady čestného obchodování\n"
+    "8. Důvěrné informace\n"
+    "9. Podvodní konáni, ochrana aktiv společnosti a účetnictví\n"
+    "10. Podplácení a korupce\n"
+    "11. Dary, pohoštění, zábava\n"
+    "12. Diskriminace a obtěžování\n"
+    "13. Nedodržování Kodexu obchodního správání\n"
+    "14. Hlásení nezákonního nebo neslučitelného správání\n"
+)
+
+ANSWER_CODE_OF_CONDUCT = "Odpověz na základě daného textu na větu.\n" 'Text: "{text}"\n'
+
+CHECK_TOXICITY = ()
+
+CHECK_TRUTHFULNESS = ()
+
+CHECK_ATTITUDE = ()
+

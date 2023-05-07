56d4ceca7bbebaec78be7e751487f5f3142b38f8
Kristina Szabova <k.szabova98@gmail.com>
2023-05-07 22:37:23 +0200

    Process message from API call

--- src/query_openai.py
+++ src/query_openai.py
@@ -8,18 +8,6 @@ def query_openai(messages):
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo", messages=messages, max_tokens=256,
     )
-    return response
+    message = response.get("choices", [{}])[0].get("message", {}).get("content")
+    return message
 
-
-messages = [
-    {
-        "role": "system",
-        "content": "Jsi chatbot na oddělení HR ve společnosti Nestlé. Jmenuješ se Adéla. Jsi nápomocná. Snažíš se maximalizovat počet uchazečů o práci ve společnosti Nestlé.",
-    },
-    {"role": "user", "content": "Jaká je tvé oblíbené jídlo?"},
-    {"role": "assistant", "content": "Mé oblíbené jídlo jsou meruňkové knedlíky."},
-    {"role": "user", "content": "Jaká je tvá oblíbená barva?"},
-]
-
-response = query_openai(messages)
-print(response)

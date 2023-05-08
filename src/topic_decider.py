9c60fb699d11d2cbf2161c27286ef8bffc497255
Kristina Szabova <k.szabova98@gmail.com>
2023-05-08 12:42:09 +0200

    Remove logging from TopicDecider

--- src/topic_decider.py
+++ src/topic_decider.py
@@ -14,22 +14,15 @@ class TopicDecider:
     def select_prompt_processor(prompt: str) -> PromptProcessor:
         # check if the query is about Code of Conduct
         query_coc = PromptConstructor.get_is_code_of_conduct_prompt(prompt)
-        print("query_coc", query_coc)
         response_coc = query_openai(query_coc)
-        print("response_coc", response_coc)
         response_coc = extract_yes_no_from_response(response_coc)
-        print(response_coc)
         if response_coc == "ano":
             return CodeOfConductProcessor()
         # check if the query is about Bot Personality
         query_personality = PromptConstructor.get_is_bot_personality_prompt(prompt)
-        print("query_personality", query_personality)
         response_personality = query_openai(query_personality)
-        print("response_personality", response_personality)
         response_personality = extract_yes_no_from_response(response_personality)
-        print(response_personality)
         if response_personality == "ano":
             return BotPersonalityProcessor()
         # otherwise, return the default processor
-        print("returning default")
         return DefaultPromptProcessor()

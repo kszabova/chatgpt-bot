27160db19f3216f8c4e11493f2ca7f771cc00722
Kristina Szabova <k.szabova98@gmail.com>
2023-05-08 11:13:52 +0200

    Implement TopicDecider

--- src/topic_decider.py
+++ src/topic_decider.py
@@ -1,9 +1,35 @@
-from prompt_processor import PromptProcessor
+from prompt_processor import (
+    PromptProcessor,
+    BotPersonalityProcessor,
+    CodeOfConductProcessor,
+    DefaultPromptProcessor,
+)
+from utils import extract_yes_no_from_response
+from query_openai import query_openai
+from prompt_constructor import PromptConstructor
 
 
 class TopicDecider:
-    def __init__(self) -> None:
-        raise NotImplemented
-
-    def select_prompt_processor(self, prompt: str) -> PromptProcessor:
-        raise NotImplemented
+    @staticmethod
+    def select_prompt_processor(prompt: str) -> PromptProcessor:
+        # check if the query is about Code of Conduct
+        query_coc = PromptConstructor.get_is_code_of_conduct_prompt(prompt)
+        print("query_coc", query_coc)
+        response_coc = query_openai(query_coc)
+        print("response_coc", response_coc)
+        response_coc = extract_yes_no_from_response(response_coc)
+        print(response_coc)
+        if response_coc == "ano":
+            return CodeOfConductProcessor()
+        # check if the query is about Bot Personality
+        query_personality = PromptConstructor.get_is_bot_personality_prompt(prompt)
+        print("query_personality", query_personality)
+        response_personality = query_openai(query_personality)
+        print("response_personality", response_personality)
+        response_personality = extract_yes_no_from_response(response_personality)
+        print(response_personality)
+        if response_personality == "ano":
+            return BotPersonalityProcessor()
+        # otherwise, return the default processor
+        print("returning default")
+        return DefaultPromptProcessor()
